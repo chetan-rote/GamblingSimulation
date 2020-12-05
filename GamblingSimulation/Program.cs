ab651b0020dedae3dd525bd514150e52b0c24620
Chetan <Chetan@DESKTOP-9SU09BS>
2020-12-05 16:48:36 +0530

    [Chetan]Add: Set stake and bet.

--- GamblingSimulation/Program.cs
+++ GamblingSimulation/Program.cs
@@ -4,6 +4,9 @@ namespace GamblingSimulation
 {
     class Program
     {
+        /*UC1 Setting Stake as 100 and Bet as 1*/
+        public const int STAKE = 100;
+        public const int BET = 1;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Gambling Simulation Problem.");
