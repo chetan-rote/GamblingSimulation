6949048b96ba98a4fef7dbda8b138bd7dd3fca9f
Chetan <Chetan@DESKTOP-9SU09BS>
2020-12-05 17:00:56 +0530

    [Chetan]Add: Win or loose.

--- GamblingSimulation/Program.cs
+++ GamblingSimulation/Program.cs
@@ -7,9 +7,20 @@ namespace GamblingSimulation
         /*UC1 Setting Stake as 100 and Bet as 1*/
         public const int STAKE = 100;
         public const int BET = 1;
+        /*UC2 Win or Loose*/
+        public static int WinOrLoose()
+        {
+            int result = new Random().Next(0, 2);
+            return result;
+        }
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Gambling Simulation Problem.");
+            WinOrLoose();
         }
     }
 }
